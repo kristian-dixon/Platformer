c253a085f8a573b6a7fc328d89e2b2e5a251d528
Kristian <kristiandixon1206@gmail.com>
2020-02-10 23:38:04 +0000

    Improved camera

--- Platformer2020/Assets/Scripts/CameraControls.cs
+++ Platformer2020/Assets/Scripts/CameraControls.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CameraControls : MonoBehaviour
 {
     public Transform player;
+    bool allowReturnToPlayerFwd = false;
 
     // Start is called before the first frame update
     void Start()
@@ -21,8 +22,30 @@ public class CameraControls : MonoBehaviour
         {
             transform.Rotate(Vector3.up, input.x);
             transform.GetChild(0).Rotate(Vector3.right, input.y);
+
+            allowReturnToPlayerFwd = false;
+
+            if (IsInvoking("UnlockCamera"))
+                CancelInvoke("UnlockCamera");
+        }
+        else if(!allowReturnToPlayerFwd)
+        {
+            if(!IsInvoking("UnlockCamera"))
+                Invoke("UnlockCamera", 3);
+        }
+        else if(allowReturnToPlayerFwd)
+        {
+            var speedMultiplier = 1.0f - ((Vector3.Dot(transform.forward, player.forward) * 0.5f) + 0.5f);
+
+            var lookAt = Vector3.RotateTowards(transform.forward, player.forward, 10.0f * speedMultiplier * Time.deltaTime, 0);
+            transform.LookAt(lookAt + transform.position);
         }
 
         transform.position = player.position;
+    }
+
+    void UnlockCamera()
+    {
+        allowReturnToPlayerFwd = true;
     }
 }
