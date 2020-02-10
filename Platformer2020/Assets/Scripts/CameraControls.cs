1d6d53660b7653ffaa97f9b9468be6e1acb800ef
Kristian <kristiandixon1206@gmail.com>
2020-02-10 23:07:56 +0000

    Initial project commit.
    Project start: Simple movement and camera controls in

---
+++ Platformer2020/Assets/Scripts/CameraControls.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControls : MonoBehaviour
+{
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxis("RightHorizontal"), Input.GetAxis("RightVertical"));
+        
+        if(input.sqrMagnitude > 0.1f)
+        {
+            transform.Rotate(Vector3.up, input.x);
+            transform.GetChild(0).Rotate(Vector3.right, input.y);
+        }
+
+        transform.position = player.position;
+    }
+}
---
+++ Platformer2020/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+    public Transform mCamera;
+    Rigidbody mRB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        if(input.sqrMagnitude > 0.1f)
+        {
+            //Orient so facing input dir
+            var inputCamSpace = mCamera.TransformVector(new Vector3(input.x, 0, input.y));
+            inputCamSpace.y = 0;
+
+            var lookDir = Vector3.RotateTowards(transform.forward, inputCamSpace, 20 * Time.deltaTime, 0);
+            
+            transform.LookAt(lookDir + transform.position);
+
+            mRB.velocity = Vector3.ClampMagnitude(mRB.velocity + inputCamSpace, 10);
+        }
+    }
+}
