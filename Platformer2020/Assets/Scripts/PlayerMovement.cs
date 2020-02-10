44a65e288b3dff433784acfa250ee21109b77557
Kristian <kristiandixon1206@gmail.com>
2020-02-10 23:47:01 +0000

    Added jump button

--- Platformer2020/Assets/Scripts/PlayerMovement.cs
+++ Platformer2020/Assets/Scripts/PlayerMovement.cs
@@ -17,6 +17,9 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        Vector3 velocityGravityless = mRB.velocity;
+        float gravity = mRB.velocity.y;
+        velocityGravityless.y = 0;
 
         if(input.sqrMagnitude > 0.1f)
         {
@@ -28,7 +31,17 @@ public class PlayerMovement : MonoBehaviour
             
             transform.LookAt(lookDir + transform.position);
 
-            mRB.velocity = Vector3.ClampMagnitude(mRB.velocity + inputCamSpace, 10);
+            velocityGravityless = Vector3.ClampMagnitude(velocityGravityless + inputCamSpace, 10);
         }
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            if (Mathf.Abs(gravity) < 0.01f)
+            {
+                gravity = 10;
+            }
+        }
+
+        mRB.velocity = velocityGravityless + Vector3.up * gravity;
     }
 }
